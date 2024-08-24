28f41989aeab9c69bfa028851aa8e5813b95bf07
scuccuru <scuccuru@contents.com>
2024-08-24 18:51:04 +0200

    add composer commands on start.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker compose up --build -d
+docker compose exec app composer install
+docker compose exec app composer
 docker compose exec app php artisan migrate
 docker compose exec app php artisan install:api

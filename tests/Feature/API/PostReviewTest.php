53ce859246ced14ce20927f210996815004d9337
scuccuru <scuccuru@contents.com>
2024-08-24 12:39:35 +0200

    post review pending status test

--- tests/Feature/API/PostReviewTest.php
+++ tests/Feature/API/PostReviewTest.php
@@ -4,27 +4,55 @@ namespace Tests\Feature\API;
 
 use App\Clients\BooksClient\OpenLibraryClient\Exceptions\WorkNotFoundException;
 use App\Services\ReviewService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Testing\TestResponse;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
 class PostReviewTest extends TestCase
 {
-    public function testWorkIdNotFound()
+    use RefreshDatabase;
+
+    public function testInitiallyHasPendingStatus(): void
+    {
+        Queue::fake();
+        $response = $this->postReview();
+
+        $this->assertDatabaseCount('reviews', 1);
+        $this->assertDatabaseHas('reviews', [
+            'work_id' => 'OL27448W',
+            'review' => 'test',
+            'score' => 6,
+        ]);
+
+        $response->assertStatus(202);
+        $response->assertExactJson([
+            'status' => 'processing...',
+        ]);
+    }
+
+    public function testWorkIdNotFound(): void
     {
         $this->partialMock(ReviewService::class, function (MockInterface $mock) {
             $mock->shouldReceive('postReview')->once()
                 ->andThrow(new WorkNotFoundException('Work ID not found.'));
         });
 
-        $response = $this->post('/api/review', [
-            'work_id' => 'OL27448W',
-            'review' => 'test',
-            'score' => 6,
-        ], ['Accept' => 'application/json']);
+        $response = $this->postReview();
 
         $response->assertStatus(422);
         $response->assertExactJson([
             'message' => 'Work ID not found.',
         ]);
+    }
+
+    private function postReview(string $workId = 'OL27448W', string $review = 'test', int $score = 6): TestResponse
+    {
+        return $this->post('/api/review', [
+            'work_id' => $workId,
+            'review' => $review,
+            'score' => $score,
+        ], ['Accept' => 'application/json']);
     }
 }

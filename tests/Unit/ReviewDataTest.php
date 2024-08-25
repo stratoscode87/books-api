d69f46a451009697da50716a764db459d6b5e92c
scuccuru <scuccuru@contents.com>
2024-08-25 13:33:42 +0200

    review data method test

--- tests/Unit/ReviewDataTest.php
+++ tests/Unit/ReviewDataTest.php
@@ -2,13 +2,51 @@
 
 namespace Tests\Unit;
 
+use App\Clients\BooksClient\Enums\CoverSize;
 use App\Clients\BooksClient\OpenLibraryClient\Exceptions\AuthorNotFoundException;
 use App\Clients\BooksClient\OpenLibraryClient\OpenLibraryClient;
 use Illuminate\Support\Facades\Http;
-use PHPUnit\Framework\TestCase;
+use Mockery\MockInterface;
+use Tests\TestCase;
 
 class ReviewDataTest extends TestCase
 {
+    public function testReviewDataMethodReturnsTheCorrectData(): void
+    {
+        $expectedResult = [
+            'title' => 'Trainspotting',
+            'description' => 'Fake description',
+            'authors' => 'Irvine Welsh',
+            'cover_url' => 'https://example.com/books/irvinewelsh.jpg',
+        ];
+
+        $mock = $this->partialMock(OpenLibraryClient::class, function (MockInterface $mock) use ($expectedResult) {
+            $mock->shouldReceive('authorNameFromWorkAuthor')
+                ->once()
+                ->andReturn($expectedResult['authors']);
+
+            $mock->shouldReceive('coverUrlFromId')
+                ->once()
+                ->andReturn($expectedResult['cover_url']);
+        });
+
+        $result = $mock->reviewData((object) [
+            'title' => 'Trainspotting',
+            'description' => 'Fake description',
+            'covers' => ['FAKECOVERID1', 'FAKECOVERID2'],
+            'authors' => (object) [
+                (object) ['author' => (object) ['key' => 'FAKEAUTHOR1']],
+            ],
+        ], CoverSize::Medium);
+
+        $this->assertEquals([
+            'title' => $expectedResult['title'],
+            'description' => $expectedResult['description'],
+            'authors' => $expectedResult['authors'],
+            'cover_img' => $expectedResult['cover_url'],
+        ], $result);
+    }
+
     public function testAuthorNameMethodReturnsTheCorrectName(): void
     {
         self::clearExistingFakes();

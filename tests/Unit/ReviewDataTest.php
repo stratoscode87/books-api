d75f75c549dc3dbb7e4e20cde8f5a2713e11d191
scuccuru <scuccuru@contents.com>
2024-08-25 12:41:17 +0200

    author not found exception test

--- tests/Unit/ReviewDataTest.php
+++ tests/Unit/ReviewDataTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Clients\BooksClient\OpenLibraryClient\Exceptions\AuthorNotFoundException;
 use App\Clients\BooksClient\OpenLibraryClient\OpenLibraryClient;
 use Illuminate\Support\Facades\Http;
 use PHPUnit\Framework\TestCase;
@@ -10,6 +11,7 @@ class ReviewDataTest extends TestCase
 {
     public function testAuthorNameMethodReturnsTheCorrectName(): void
     {
+        self::clearExistingFakes();
         $openLibraryClient = new OpenLibraryClient;
 
         //Fake the result of Open Library author API
@@ -17,9 +19,30 @@ class ReviewDataTest extends TestCase
         Http::fake([
             $openLibraryClient->getUrl().'authors/*' => Http::response($openLibraryResponse, 200, ['Headers']),
         ]);
-
-        $name = $openLibraryClient->authorNameFromWorkAuthor('OL23919A');
+        $name = $openLibraryClient->authorNameFromWorkAuthor('FAKEAUTHORID');
 
         $this->assertEquals('Irvine Welsh', $name);
+    }
+
+    public function testAuthorNameNotFoundException(): void
+    {
+        self::clearExistingFakes();
+        $this->expectException(AuthorNotFoundException::class);
+        $openLibraryClient = new OpenLibraryClient;
+
+        //Fake Open Library author not found response
+        Http::fake([
+            $openLibraryClient->getUrl().'authors/*' => Http::response(['error' => 'notfound'], 200, ['Headers']),
+        ]);
+
+        $openLibraryClient->authorNameFromWorkAuthor('FAKEAUTHORID');
+    }
+
+    private static function clearExistingFakes(): void
+    {
+        $reflection = new \ReflectionObject(Http::getFacadeRoot());
+        $property = $reflection->getProperty('stubCallbacks');
+        $property->setAccessible(true);
+        $property->setValue(Http::getFacadeRoot(), collect());
     }
 }
